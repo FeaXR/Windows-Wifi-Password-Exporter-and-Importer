6b98b61a87bb35a126c59c5a27f38f0ee22f245c
FeaXR <55257249+FeaXR@users.noreply.github.com>
2023-05-09 19:29:51 +0200

    remove comment

--- wifi_export.ps1
+++ wifi_export.ps1
@@ -1,4 +1,3 @@
-# powershell -ExecutionPolicy ByPass -File .\export.ps1
 # $XmlDirectory = Get-Location     # If you want to run it at the folder you are currently in
 $XmlDirectory = $PSScriptRoot      # If you want to run it at the folder the script is saved in
 
