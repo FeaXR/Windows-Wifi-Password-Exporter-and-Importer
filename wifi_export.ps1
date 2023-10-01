f7128fa2c982168d23a01c934c9c1c4dcda04bab
FeaXR <55257249+FeaXR@users.noreply.github.com>
2023-10-01 14:43:19 +0200

    Update with input params

--- wifi_export.ps1
+++ wifi_export.ps1
@@ -1,4 +1,19 @@
-# $XmlDirectory = Get-Location     # If you want to run it at the folder you are currently in
-$XmlDirectory = $PSScriptRoot      # If you want to run it at the folder the script is saved in
+# powershell -ExecutionPolicy ByPass -File .\export.ps1
 
-Netsh wlan export profile key=clear folder=$XmlDirectory
+param(
+    [switch]$UseCurrentLocation
+)
+
+if ($UseCurrentLocation) {
+    $XmlDirectory = Get-Location    # If you want to run it at the folder you are currently in
+} else {
+    $XmlDirectory = $PSScriptRoot   # If you want to run it at the folder the script is saved in
+}
+
+
+$CurrentDate = Get-Date -Format "yyyy-MM-dd"
+$FolderName = "$CurrentDate wifi psw"
+$FolderPath = Join-Path -Path $XmlDirectory -ChildPath $FolderName
+New-Item -ItemType Directory -Path $FolderPath
+
+Netsh wlan export profile key=clear folder=$FolderPath
--- wifi_import.ps1
+++ wifi_import.ps1
@@ -1,3 +1,18 @@
-# $XmlDirectory = Get-Location     # If you want to run it at the folder you are currently in
-$XmlDirectory = $PSScriptRoot      # If you want to run it at the folder the script is saved in
+# powershell -ExecutionPolicy ByPass -File .\import.ps1
+
+param(
+    [switch]$UseCurrentLocation,
+    [string]$Path = $null
+)
+
+if ($UseCurrentLocation) {
+    $XmlDirectory = Get-Location    # If you want to run it at the folder you are currently in
+} else {
+    $XmlDirectory = $PSScriptRoot   # If you want to run it at the folder the script is saved in
+}
+
+if ($Path -ne $null) {
+    $XmlDirectory = $Path  # Use the specified path if provided
+}
+
 Get-ChildItem $XmlDirectory | Where-Object {$_.extension -eq ".xml"} | ForEach-Object {netsh wlan add profile filename=("$XmlDirectory\$($_.name)")}
