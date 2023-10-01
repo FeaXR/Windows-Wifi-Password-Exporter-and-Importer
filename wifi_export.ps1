11f3056ff9878d0c4596fdef0278ae2747e6a739
FeaXR <55257249+FeaXR@users.noreply.github.com>
2023-10-01 14:54:53 +0200

    Update wifi_export.ps1

--- wifi_export.ps1
+++ wifi_export.ps1
@@ -1,13 +1,18 @@
 # powershell -ExecutionPolicy ByPass -File .\export.ps1
 
 param(
-    [switch]$UseCurrentLocation
+    [switch]$UseCurrentLocation,    
+    [string]$Path = $null
 )
 
 if ($UseCurrentLocation) {
     $XmlDirectory = Get-Location    # If you want to run it at the folder you are currently in
 } else {
     $XmlDirectory = $PSScriptRoot   # If you want to run it at the folder the script is saved in
+}
+
+if ($Path -ne $null) {
+    $XmlDirectory = $Path  # Use the specified path if provided
 }
 
 
